9f35882867efd5cc32281b03cf04ea129bfef030
Emelyanov Roman <wohagy@gmail.com>
2022-01-24 20:48:14 +0300

    first commit

---
+++ BMI Calculator/Controllers/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by wohagy on 24/01/2022.
+//  Copyright © 2022 wohagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
